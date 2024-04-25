4d7b79514e3937bce12bf47cf16058adf1121264
Mwanambuyu Akamandisa <amwaka98@gmail.com>
2024-04-25 15:11:31 +0200

    updated the header

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     extend: {
       screens: {
         'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
-        'tallscreen': { 'raw': '(min-aspect-ratio: 13/20)' },
+        'tallscreen': { 'raw': '(max-aspect-ratio: 13/20)' },
       },
     keyframes: {
       'open-menu': {
